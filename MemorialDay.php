43d4e32e45603ed2017436a6043b35c62a43d94d
Benny <benny.think@gmail.com>
2017-07-22 15:28:18 +0800

    test phase1

--- MemorialDay.php
+++ MemorialDay.php
@@ -25,6 +25,7 @@ Author URI: http://www.bennythink.com
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+date_default_timezone_set('Asia/Shanghai');
 
 
 function memorial_day(){?>
@@ -41,21 +42,56 @@ function memorial_day(){?>
 //code...
 add_action ( 'wp_head', 'memorial_day' );
 
+function test(){
 
-/** 第1步：定义添加菜单选项的函数 */
-function my_plugin_menu() {
-	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+echo 'start testing';
+	echo get_option('bt_group');
+
+}
+//add_action('wp_loaded','test');
+
+
+
+
+function add_qzone_settings_link($links, $file) {
+	static $this_plugin;
+	if (!$this_plugin) $this_plugin = plugin_basename(__FILE__);
+
+	if ($file == $this_plugin){
+		$settings_link = '<a href="'.wp_nonce_url("admin.php?page=my-unique-identifier").'">设置</a>';
+		array_unshift($links, $settings_link);
+	}
+	return $links;
+}
+add_filter('plugin_action_links', 'add_qzone_settings_link', 10, 2 );
+
+
+
+
+
+add_action( 'admin_menu', 'bt_create_menu' );
+
+function bt_create_menu() {
+
+	//创建顶级菜单
+	add_menu_page(
+		'纪念日插件设置',
+		'纪念日',
+		'manage_options',
+		'bt_MemorialDay' ,
+		'bt_settings_page',
+		plugins_url( '/icon.png', __FILE__ )
+	);
 }
 
-/** 第2步：将函数注册到钩子中 */
-add_action( 'admin_menu', 'my_plugin_menu' );
 
-/** 第3步：定义选项被点击时打开的页面 */
-function my_plugin_options() {
-	if ( ! current_user_can( 'manage_options' ) ) {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-	}
 
-echo 'settings page';
+
+function bt_settings_page(){
+
+
+	echo 'settings go here';
+
+
 
 }
