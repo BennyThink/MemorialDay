8853846c9a742f6d8c0a1e60a37f9be8509b4b51
Benny <benny.think@gmail.com>
2017-07-22 17:02:09 +0800

    v0.2

--- MemorialDay.php
+++ MemorialDay.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: 纪念日
 Plugin URI:
-Description: 在指定日期将网站改成黑白色来纪念逝去的生命
-Version: 0.01
+Description: 在指定日期将网站改成黑白色来缅怀逝去的生命
+Version: 0.2
 Author: Benny小土豆
 Author URI: http://www.bennythink.com
 */
@@ -24,74 +24,118 @@ Author URI: http://www.bennythink.com
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+//This plugin is designed for WordPress
 
-date_default_timezone_set('Asia/Shanghai');
+date_default_timezone_set( 'Asia/Shanghai' );
 
+add_action( 'wp_head', 'memorial_day' );
+function memorial_day() {
+	$options     = get_option( 'plugin_options' );
+	$theme_color = $options['text_string'];
+	$custom_date = $options['text_date'];
 
-function memorial_day(){?>
-	<style type="text/css">
-			<!--
-	html {
-		filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
-		-webkit-filter: grayscale(100%);
-			}
+	if ( strstr( $custom_date, date( 'm-d', time() ) ) ):?>
 
-			-->
-		</style>
+        <meta name="theme-color" content="#757575">
+        <style type="text/css">
+            <!--
+            html {
+                filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
+                -webkit-filter: grayscale(100%);
+            }
+
+            -->
+        </style>
+	<?php else: ?>
+        <meta name="theme-color" content="<?= $theme_color; ?>">
+
+	<?php endif; ?>
 <?php }
-//code...
-add_action ( 'wp_head', 'memorial_day' );
-
-function test(){
-
-echo 'start testing';
-	echo get_option('bt_group');
-
-}
-//add_action('wp_loaded','test');
 
 
-
-
-function add_qzone_settings_link($links, $file) {
+function add_qzone_settings_link( $links, $file ) {
 	static $this_plugin;
-	if (!$this_plugin) $this_plugin = plugin_basename(__FILE__);
-
-	if ($file == $this_plugin){
-		$settings_link = '<a href="'.wp_nonce_url("admin.php?page=my-unique-identifier").'">设置</a>';
-		array_unshift($links, $settings_link);
+	if ( ! $this_plugin ) {
+		$this_plugin = plugin_basename( __FILE__ );
 	}
+
+	if ( $file == $this_plugin ) {
+		$settings_link = '<a href="' . wp_nonce_url( "admin.php?page=MemorialDay" ) . '">设置</a>';
+		array_unshift( $links, $settings_link );
+	}
+
 	return $links;
 }
-add_filter('plugin_action_links', 'add_qzone_settings_link', 10, 2 );
+
+add_filter( 'plugin_action_links', 'add_qzone_settings_link', 10, 2 );
 
 
-
-
-
-add_action( 'admin_menu', 'bt_create_menu' );
-
-function bt_create_menu() {
-
-	//创建顶级菜单
-	add_menu_page(
-		'纪念日插件设置',
-		'纪念日',
+add_action( 'admin_menu', 'plugin_admin_add_page' );
+function plugin_admin_add_page() {
+	add_options_page(
+		'纪念日 设置页面',
+		'纪念日  设置',
 		'manage_options',
-		'bt_MemorialDay' ,
-		'bt_settings_page',
-		plugins_url( '/icon.png', __FILE__ )
+		'MemorialDay',
+		'plugin_options_page' );
+}
+
+function plugin_options_page() {
+	?>
+    <div>
+        <h2>以此来缅怀那些逝去的生命</h2>
+        请设置你需要的日期（形如07-13）与主题颜色（theme-color）
+        <form action="options.php" method="post">
+			<?php settings_fields( 'plugin_options' ); ?>
+			<?php do_settings_sections( 'plugin' ); ?>
+
+            <input name="Submit" type="submit" value="<?php esc_attr_e( 'Save Changes' ); ?>"/>
+        </form>
+    </div>
+
+	<?php
+}
+
+
+add_action( 'admin_init', 'plugin_admin_init' );
+function plugin_admin_init() {
+	register_setting(
+		'plugin_options',
+		'plugin_options',
+		'plugin_options_validate' );
+
+	add_settings_section(
+		'plugin_main',
+		'日期设置，一行一个',
+		'plugin_date_text',
+		'plugin'
 	);
+
+
+	add_settings_section(
+		'plugin_main2',
+		'theme-color，十六进制不需要带#',
+		'plugin_color_text',
+		'plugin'
+	);
+
+
+}
+
+function plugin_date_text() {
+	$options = get_option( 'plugin_options' );
+	echo "<textarea name='plugin_options[text_date]'>" . $options['text_date'] . "</textarea>";
+
+}
+
+function plugin_color_text() {
+	$options = get_option( 'plugin_options' );
+	echo "<input id='color_string' name='plugin_options[text_string]' size='40' 
+type='text' value='{$options['text_string']}' />" . "<br>" . "<br>";
+
+
 }
 
 
 
 
-function bt_settings_page(){
-
-
-	echo 'settings go here';
-
-
-
-}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-MemorialDay
+# MemorialDay
+一个可以在指定日期让你的网站变成黑白模式的纪念日插件
+
+## 为啥要搞这玩意 ##
+> What is dead may never die.
+
+以此来缅怀那些逝去的生命。
+
+## 使用方法 ##
+将`MemorialDay.php`放到`wp-content/plugins`，后台中启用，设置日期和theme-color即可
+
+## Q&A ##
+### theme-color是什么，我该怎么设置 ###
+此项值用来控制Android Chrome通知栏变色，如果你的主题不带类似下面这样的头部：
+`<meta name="theme-color" content="#299981">`
+那就把这个十六进制颜色值填进去吧，正常的时候就会通知栏变色；
+如果你的主题带这样的头部，那.....
+
+### License ###
+GPLv2
